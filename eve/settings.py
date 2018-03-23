d79d2e06bfdb6cd54b6dcfc7aad7c3909b401297
Daniel Cirstea <daniel.cirstea96@gmail.com>
2018-03-23 10:30:46 +0200

    Update settings.py

--- eve/settings.py
+++ eve/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
